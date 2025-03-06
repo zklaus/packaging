54d2096825a5c2d958d9322df7deb0089cf063d4
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-03-06 15:47:52 +0100

    Turn comparison bool into string

--- src/packaging/markers.py
+++ src/packaging/markers.py
@@ -243,7 +243,7 @@ def default_environment() -> Environment:
     iver = format_full_version(sys.implementation.version)
     implementation_name = sys.implementation.name
     return {
-        "free_threading": "t" in sys.abiflags,
+        "free_threading": str("t" in sys.abiflags),
         "implementation_name": implementation_name,
         "implementation_version": iver,
         "os_name": os.name,
