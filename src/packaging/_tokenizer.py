e2c0df6cec3c0916afcd92e068d9a89c95f6f0ec
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-03-11 10:37:32 +0100

    Move abi_features to sys from sysconfig

--- src/packaging/_tokenizer.py
+++ src/packaging/_tokenizer.py
@@ -68,7 +68,7 @@ DEFAULT_RULES: dict[str, str | re.Pattern[str]] = {
                 |platform[._](version|machine|python_implementation)
                 |python_implementation
                 |implementation_(name|version)
-                |sysconfig_abi_features
+                |sys_abi_features
                 |extra
             )\b
         """,
